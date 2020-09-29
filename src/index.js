b84fd06384a8840b7a297fb4edc0113c84bb4175
Alexander Silchenko <san4ys.1998@gmail.com>
2020-09-29 19:30:40 +0300

    feat: implement min function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+    if (!array || !array.length) return 0;
+    return Math.min(...array);
+};
 
 exports.max = function max (array) {
   return 0;
